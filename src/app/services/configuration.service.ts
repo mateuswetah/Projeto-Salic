fa1ecc38c3f432764c5588084e0cdb5e0350fa5b
mateuswetah <mateus.m.luna@gmail.com>
2017-02-24 12:55:24 -0300

    Troca a URL para versão sem prefixo http.

--- src/app/services/configuration.service.ts
+++ src/app/services/configuration.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ConfigurationService {
 
-   public ApiUrl: String = 'http://hmg.api.salic.cultura.gov.br/beta/';
+   public ApiUrl: String = '//hmg.api.salic.cultura.gov.br/beta/';
 
 }
