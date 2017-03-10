69393ce3260d1a36dd34ddeefeebd7dad6a8d15a
mateuswetah <mateus.m.luna@gmail.com>
2017-03-10 15:59:47 -0300

    Auto-generated commit

--- src/app/services/configuration.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class ConfigurationService {
-
-   public ApiUrl: String = 'http://hmg.api.salic.cultura.gov.br/beta/';
-   public limitResultados = 20;
-}
